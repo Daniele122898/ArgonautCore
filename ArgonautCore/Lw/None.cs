e04dfa86015c0b069e7410aaf5139da7385f7493
Serenity <d.palbanese@gmail.com>
2020-05-11 20:28:27 +0200

    Added none type

---
+++ ArgonautCore/Lw/None.cs
@@ -0,0 +1,9 @@
+﻿namespace ArgonautCore.Lw
+{
+    // Inspired / Copied from https://github.com/sn0w bcs it's better than what i could write by myself kekw
+    
+    /// <summary>
+    /// Represents the absence of any value since you cannot use void as a type-param.
+    /// </summary>
+    public struct None { }
+}
