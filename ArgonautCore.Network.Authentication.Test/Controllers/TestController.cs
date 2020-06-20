1e90fe6e679df82ad948ebcc244bfa33d961080f
Serenity <d.palbanese@gmail.com>
2020-06-20 20:54:46 +0200

    Added test project

---
+++ ArgonautCore.Network.Authentication.Test/Controllers/TestController.cs
@@ -0,0 +1,27 @@
+﻿
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArgonautCore.Network.Authentication.Test.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult TestNoAuthentication()
+        {
+            return Ok();
+        }
+
+        [HttpGet("auth")]
+        [Authorize]
+        public IActionResult TestWithAuthentication()
+        {
+            int userId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value);
+            string username = User.FindFirst(ClaimTypes.Name).Value;
+            return Ok(new {userId, username});
+        }
+    }
+}
